80ce000983a926978b40bca236c4060c47c4a19d
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-03 00:06:35 +0200

    feat: create AdsRouter

---
+++ LBCTechnicalTest/Ads/AdsRouter.swift
@@ -0,0 +1,18 @@
+//
+//  AdsRouter.swift
+//  LBCTechnicalTest
+//
+//  Created by Loic Mazuc on 03/05/2023.
+//
+
+import UIKit
+
+final class AdsRouter: DefaultRouter {
+    func makeRootViewController() -> UIViewController {
+        let router = AdsRouter(rootTransition: EmptyTransition())
+        let rootViewController = ViewController()
+        router.rootViewController = rootViewController
+
+        return UINavigationController(rootViewController: rootViewController)
+    }
+}
--- LBCTechnicalTest/SceneDelegate.swift
+++ LBCTechnicalTest/SceneDelegate.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var adsRouter: AdsRouter?
 
     func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        adsRouter = AdsRouter(rootTransition: EmptyTransition())
+
+        guard let windowScene = (scene as? UIWindowScene),
+              let rootViewController = adsRouter?.makeRootViewController() else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = rootViewController
         window.makeKeyAndVisible()
         self.window = window
     }
