b088d759ea7d6440eed5667213dec2f31bc31c9b
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-02 23:12:56 +0200

    feat: remove storyboard

--- LBCTechnicalTestTests/LBCTechnicalTestTests.swift
+++ LBCTechnicalTestTests/LBCTechnicalTestTests.swift
@@ -5,11 +5,10 @@
 //  Created by Loic Mazuc on 02/05/2023.
 //
 
-import XCTest
 @testable import LBCTechnicalTest
+import XCTest
 
 final class LBCTechnicalTestTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class LBCTechnicalTestTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- LBCTechnicalTestUITests/LBCTechnicalTestUITestsLaunchTests.swift
+++ LBCTechnicalTestUITests/LBCTechnicalTestUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class LBCTechnicalTestUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
