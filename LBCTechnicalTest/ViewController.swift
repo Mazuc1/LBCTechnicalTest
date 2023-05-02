5c70e5cfc5411e98c25af2decb7031d91d14497c
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-02 23:05:46 +0200

    Initial commit

---
+++ LBCTechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LBCTechnicalTest
+//
+//  Created by Loic Mazuc on 02/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
