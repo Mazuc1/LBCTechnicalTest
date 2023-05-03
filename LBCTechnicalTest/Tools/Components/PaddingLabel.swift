167e8fe5ce73fd1b4694f18e34ec3155adaddbb3
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-04 00:04:44 +0200

    feat: create custom UILabel to be able to set padding

---
+++ LBCTechnicalTest/Tools/Components/PaddingLabel.swift
@@ -0,0 +1,59 @@
+//
+//  PaddingLabel.swift
+//  LBCTechnicalTest
+//
+//  Created by Loic Mazuc on 04/05/2023.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+    
+    // MARK: - Properties
+    
+    var topInset: CGFloat
+    var bottomInset: CGFloat
+    var leftInset: CGFloat
+    var rightInset: CGFloat
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+
+    override var bounds: CGRect {
+        didSet {
+            preferredMaxLayoutWidth = bounds.width - (leftInset + rightInset)
+        }
+    }
+    
+    // MARK: - Init
+
+    init(topInset: CGFloat,
+         bottomInset: CGFloat,
+         leftInset: CGFloat,
+         rightInset: CGFloat)
+    {
+        self.topInset = topInset
+        self.bottomInset = bottomInset
+        self.leftInset = leftInset
+        self.rightInset = rightInset
+        super.init(frame: .zero)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset,
+                                  left: leftInset,
+                                  bottom: bottomInset,
+                                  right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+}
