65458047c8fbb934d0c1393c4d024d56e51953ce
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-04 13:19:10 +0200

    feat: create AdsFetchingService

---
+++ LBCTechnicalTest/Ads/AdsFetchingService.swift
@@ -0,0 +1,45 @@
+//
+//  AdsFetchingService.swift
+//  LBCTechnicalTest
+//
+//  Created by Loic Mazuc on 04/05/2023.
+//
+
+import Combine
+import Foundation
+
+protocol AdsFetchingServiceProtocol: AnyObject {
+    func fetchAds() -> AnyPublisher<[Ad], Error>
+}
+
+final class AdsFetchingService: AdsFetchingServiceProtocol {
+    //  MARK: - Properties
+
+    private let listEndPoint = "https://raw.githubusercontent.com/leboncoin/paperclip/master/listing.json"
+    private let categoriesEndPoint = "https://raw.githubusercontent.com/leboncoin/paperclip/master/categories.json"
+
+    enum AdsFetchingServiceError: Error {
+        case cannotBuildURL
+    }
+
+    let urlSession: URLSession
+
+    // MARK: - Init
+
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+
+    // MARK: - Methods
+
+    func fetchAds() -> AnyPublisher<[Ad], Error> {
+        guard let url = URL(string: listEndPoint) else {
+            return Fail(error: AdsFetchingServiceError.cannotBuildURL).eraseToAnyPublisher()
+        }
+
+        return urlSession.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Ad].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
