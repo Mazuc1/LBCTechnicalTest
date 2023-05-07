d7f0c0388b7b5a72bbed9800b15ed44ab17a93ef
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-08 00:41:22 +0200

    test: add error enum and decodable helpers

---
+++ LBCTechnicalTestTests/AdsFetchingServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  AdsFetchingServiceTests.swift
+//  LBCTechnicalTestTests
+//
+//  Created by Loic Mazuc on 02/05/2023.
+//
+
+@testable import LBCTechnicalTest
+import XCTest
+
+final class AdsFetchingServiceTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+}
---
+++ LBCTechnicalTestTests/Helpers/TestsError.swift
@@ -0,0 +1,12 @@
+//
+//  TestError.swift
+//  LBCTechnicalTestTests
+//
+//  Created by Loic Mazuc on 08/05/2023.
+//
+
+import Foundation
+
+enum TestsError: Error {
+    case anyError
+}
