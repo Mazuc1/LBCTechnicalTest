3e9f7c2b1386bfe504d7135480aeede2aededd3f
Loïc Mazuc <mazuc.loic@icloud.com>
2023-05-08 15:49:54 +0200

    test: create AdsViewControllerTests

---
+++ LBCTechnicalTestTests/AdsViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AdsViewControllerTests.swift
+//  LBCTechnicalTestTests
+//
+//  Created by Loic Mazuc on 08/05/2023.
+//
+
+@testable import LBCTechnicalTest
+import XCTest
+
+final class AdsViewControllerTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+}
